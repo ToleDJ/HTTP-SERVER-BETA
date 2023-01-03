204f9641ae1a196b4fc21ed53e137fb0af9d7fef
ToleDJ <106608719+ToleDJ@users.noreply.github.com>
2023-01-03 16:06:06 +0200

    Add files via upload

---
+++ HTTP SERVER/module1.py
@@ -0,0 +1,21 @@
+import socket
+import HTTP_Server
+import sqlite3
+import SQLtoJson
+
+# conn = sqlite3.connect("WebRoot/db/movie1.db")
+# c = conn.cursor()
+# c.execute("""CREATE TABLE movie1(
+#             comments text
+#             )""")
+# conn.commit()
+# conn.close()
+
+class DB:
+    def __init__(self, data):
+        conn = sqlite3.connect("WebRoot/db/movie1.db")
+        c = conn.cursor()
+        c.execute("INSERT INTO movie1 VALUES ('{}')".format(data))
+        conn.commit()
+        conn.close()
+        SQLtoJson.SQLtoJson()
